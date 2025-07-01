bfc8a7a4118458a73ac02fd4d02ee91b2153aedc
Влад Полуведько <vladpoluvedko@MacBook-Pro-Vlad.local>
2025-07-01 13:08:31 +0100

    Refaktoryzacja: pełna integracja TripModel w kontrolerze

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,50 +1,52 @@
-// controllers/TripController.js
-const TripModel = require('../models/TripModel');
 const path = require('path');
+const TripModel = require('../models/TripModel');
 
 exports.getTriptsPage = (req, res) => {
-    const trips = TripModel.getAll();
-    res.render('triplist', { trips });
+  const trips = TripModel.getAll();
+  res.render('triplist', { trips });
 };
 
+
 exports.getAddTripPage = (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'formNew.html'));
+  res.sendFile(path.join(__dirname, '..', 'views', 'formNew.html'));
 };
 
 exports.addTrip = (req, res) => {
-    const { description, date, transport, hotel } = req.body;
-    TripModel.add(description, date, transport, hotel);
-    res.redirect('/');
+  const { description, date, transport, hotel } = req.body;
+  TripModel.add(description, date, transport, hotel);
+  res.redirect('/');
 };
 
+
 exports.getEditTripPage = (req, res) => {
-    const id = req.params.id;
-    const trip = TripModel.getById(id);
-    if (!trip) {
-        res.status(404).send('Trip not found');
-    } else {
-        trip.id = id; // передаємо id для форми
-        res.render('formUpdate', { trip });
-    }
+  const id = req.params.id;
+  const trip = TripModel.getById(id);
+  if (!trip) {
+    res.status(404).send('Trip not found');
+  } else {
+    trip.id = id; 
+    res.render('formUpdate', { trip });
+  }
 };
 
 exports.updateTrip = (req, res) => {
-    const id = req.params.id;
-    const { description, date, transport, hotel } = req.body;
-    const updated = TripModel.update(id, description, date, transport, hotel);
-    if (!updated) {
-        res.status(404).send('Trip not found');
-    } else {
-        res.redirect('/');
-    }
+  const id = req.params.id;
+  const { description, date, transport, hotel } = req.body;
+  const updated = TripModel.update(id, description, date, transport, hotel);
+  if (!updated) {
+    res.status(404).send('Trip not found');
+  } else {
+    res.redirect('/');
+  }
 };
 
+
 exports.deleteTrip = (req, res) => {
-    const id = req.params.id;
-    const deleted = TripModel.delete(id);
-    if (!deleted) {
-        res.status(404).send('Trip not found');
-    } else {
-        res.redirect('/');
-    }
+  const id = req.params.id;
+  const deleted = TripModel.delete(id);
+  if (!deleted) {
+    res.status(404).send('Trip not found');
+  } else {
+    res.redirect('/');
+  }
 };
