7f198d966defefa78f38200f397a99aacba1ad07
Влад Полуведько <vladpoluvedko@MacBook-Pro-Vlad.local>
2025-07-01 13:03:04 +0100

    Finalna struktura MVC: dodano routes, modele i widoki

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-// app.js
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
+
 const tripRoutes = require('./routes/trips');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', tripRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
--- routes/trips.js
+++ routes/trips.js
@@ -1,13 +1,14 @@
 // routes/trips.js
 const express = require('express');
 const router = express.Router();
-const tripController = require('../controllers/tripController');
+const TripController = require('../controllers/tripController');
 
-router.get('/', tripController.getTriptsPage);
-router.get('/new', tripController.getAddTripPage);
-router.post('/new', tripController.addTrip);
-router.get('/edit/:id', tripController.getEditTripPage);
-router.post('/edit/:id', tripController.updateTrip);
-router.post('/delete/:id', tripController.deleteTrip);
+router.get('/', TripController.getTriptsPage);
+router.get('/new', TripController.getAddTripPage);
+router.post('/new', TripController.addTrip);
+router.get('/edit/:id', TripController.getEditTripPage);
+router.post('/edit/:id', TripController.updateTrip);
+router.post('/delete/:id', TripController.deleteTrip);
 
 module.exports = router;
+
